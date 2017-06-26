2fdc33c3b676a2cdd5a7269a08a1f03998a3bc6e
Luop <luop90@gmx.com>
2017-06-26 10:32:00 -0600

    Added github link to @author

--- scripts/temp-mail.org.js
+++ scripts/temp-mail.org.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Reks temp-mail's anti-adblock.
-// @author       Luop90
+// @author       Luop90 (github.com/luop90)
 // @match        https://temp-mail.org/*
 // @grant        none
 // ==/UserScript==
